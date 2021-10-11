f2568524760044ded77fa1b3e66cf90670b7a873
SoloSky2020 <wdzimm@icloud.com>
2021-10-11 18:30:12 -0500

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
-}
 import React from "react";
 
 var img = new Image();
